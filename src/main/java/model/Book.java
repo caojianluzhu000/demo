13bd40421ebffedefb359bad2e74dbbc07fe55e8
yixuan.chen <chenyixuan09@gmail.com>
2017-12-10 21:48:27 -0500

    Initial commit

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,85 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String publisher;
+
+    @ManyToMany
+    private Set<Author> authorSet;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String publisher, Set<Author> authorSet) {
+        this.id = id;
+        this.title = title;
+        this.publisher = publisher;
+        this.authorSet = authorSet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthorSet() {
+        return authorSet;
+    }
+
+    public void setAuthorSet(Set<Author> authorSet) {
+        this.authorSet = authorSet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authorSet=" + authorSet +
+                '}';
+    }
+}
